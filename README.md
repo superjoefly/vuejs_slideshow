dfec0258bbdbf5a3ed5fbc81f3e6cb3ab8f6ebbb
superjoefly <superjoefly@gmail.com>
2017-10-31 20:52:46 -0600

    Updated to use Animate.css

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # Vuejs Slideshow
 
-An image slideshow built with Vuejs, using Velocity for the transitions.
+An Image Slideshow built with Vuejs, using Animate.css for the transitions.
+
+## View Live
+
+To view this project live, go to my [Vuejs Slideshow](https://joefly.site/pages/projects/vuejs_slideshow/index.html) page.
 
 ## Getting Started
 
-Just clone or download the zip and open the index.html file in your browser...simple! You can replace the images with your own, and use different Velocity animations.
+Just clone or download the zip and open the index.html file in your browser...simple! You can replace the images with your own, and use different Animate.css transitions.
 
+## AnimateCSS
 
-## To view this project live, go to my  [Vuejs Slideshow](https://joefly.site/pages/projects/vuejs_slideshow/index.html) page.
+A list of available transitions is available [here](https://daneden.github.io/animate.css/)
--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 Vue.component('my-slideshow', {
   template: `
-  <div>
-      <h2 class="w3-center">Vuejs Automatic Slideshow</h2>
+    <div>
+      <h2 class="w3-center">Vuejs Slideshow</h2>
+        <br/>
+        <br/>
+              <img src="images/scene1.jpg" alt="scene one" style="width: 100%" class="slides animated flipInY" />
 
-      <div id="app" class="w3-content w3-section" style="max-width: 800px">
-        <img src="images/scene1.jpg" alt="scene one" style="width: 100%" class="slides w3-animate-zoom" />
+              <img src="images/scene2.jpg" alt="scene two" style="width: 100%" class="slides animated bounceInRight" />
 
-        <img src="images/scene2.jpg" alt="scene two" style="width: 100%" class="slides w3-animate-right" />
+              <img src="images/scene3.jpg" alt="scene three" style="width: 100%" class="slides animated wobble" />
 
-        <img src="images/scene3.jpg" alt="scene three" style="width: 100%" class="slides w3-animate-left" />
+              <img src="images/scene4.jpg" alt="scene four" style="width: 100%" class="slides animated jello" />
 
-        <img src="images/scene4.jpg" alt="scene four" style="width: 100%" class="slides w3-animate-bottom" />
-
-        <img src="images/scene5.jpg" alt="scene five" style="width: 100%" class="slides w3-animate-opacity" />
+              <img src="images/scene5.jpg" alt="scene five" style="width: 100%" class="slides animated slideInLeft" />
       </div>
-  </div>
   `
-})
+});
+
 
 new Vue({
   el: '#app',
@@ -35,7 +35,7 @@ new Vue({
       }
       this.myIndex++;
       if (this.myIndex > x.length) {
-        this.myIndex = 1
+        this.myIndex = 1;
       }
       x[this.myIndex - 1].style.display = "block";
         setTimeout(this.carousel, 3000);
@@ -45,4 +45,4 @@ new Vue({
     mounted() {
       this.carousel();
     }
-})
+});
